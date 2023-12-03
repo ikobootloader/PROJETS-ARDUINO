78a852054cb1779c23df1034fa6886d1289be96b
Frédérick Murat <63266419+ikobootloader@users.noreply.github.com>
2023-12-03 12:32:18 +0100

    Add files via upload

--- libraries/shift_register/shift_register.h
+++ libraries/shift_register/shift_register.h
@@ -6,18 +6,18 @@
 class SHIFT_REGISTER {
 
 	public:
-		SHIFT_REGISTER(uint8_t);
+		//SHIFT_REGISTER(uint8_t);//TODO:permettre plusieurs registres à décalage
 		SHIFT_REGISTER();
 		~SHIFT_REGISTER();	
 		void setupPins();
 		void setupPins(const uint8_t ,const uint8_t ,const uint8_t );		
 		
 		uint8_t pin_DS = 8;  
-		uint8_t pin_ST_CP = 10;
-		uint8_t pin_SH_CP = 11;
+		uint8_t pin_ST_CP = 4;
+		uint8_t pin_SH_CP = 2;
 		uint8_t Nb74hc595 = 1; // Nombre de registre a decalage utilise
 		uint8_t NbPins = 8; // Nombre total de pin de registre a decalage
-		boolean *registres; // Array contenant des bits de la taille du nombre de pin 74hc595
+		boolean registres[8]; // Array contenant des bits de la taille du nombre de pin 74hc595
 		
 		void effacerRegistres();
 		void pinRegistre(int, int);
@@ -25,8 +25,8 @@ class SHIFT_REGISTER {
 		
 	private:
 		const uint8_t _pin_DS = 8;   
-		const uint8_t _pin_ST_CP = 10;
-		const uint8_t _pin_SH_CP = 11;
+		const uint8_t _pin_ST_CP = 4;
+		const uint8_t _pin_SH_CP = 2;
 		const uint8_t _Nb74hc595 = 1;
 		const uint8_t _NbPins = 8;
 
