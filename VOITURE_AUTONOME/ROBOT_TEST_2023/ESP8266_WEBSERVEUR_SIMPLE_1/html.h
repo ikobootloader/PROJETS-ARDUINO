483df9d3791af9437b4e7679e687052fa64a7c51
Frédérick Murat <63266419+ikobootloader@users.noreply.github.com>
2023-12-04 21:59:49 +0100

    Add files via upload

--- VOITURE_AUTONOME/ROBOT_TEST_2023/ESP8266_WEBSERVEUR_SIMPLE_1/html.h
+++ VOITURE_AUTONOME/ROBOT_TEST_2023/ESP8266_WEBSERVEUR_SIMPLE_1/html.h
@@ -10,139 +10,164 @@ const char html[] PROGMEM = R"=====(
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Parcours du Véhicule</title>
         <style>
-        body, html {
-            height: 100%;
-            margin: 0;
-            overflow: hidden;
-          background-color: black;
-        }
+          body, html {
+              margin: 0;
+              padding: 0;
+              height: 100%;
+          }
 
-        #map {
-            position: absolute;
-            top: 40%;
-            left: 40%;
-            transform: translate(-50%, -50%);
-            /*border: 1px solid #ccc;*/
-            /*overflow: hidden; *//* Empêche les points de déborder du cadre visible */
-        }
+          #map {
+              position: relative;
+              border: 1px solid #ccc;
+              overflow: hidden;
+              width: 100vw; /* Utilise la largeur totale de la fenêtre */
+              height: 100vh; /* Utilise la hauteur totale de la fenêtre */
+          }
 
-        #vehicle {
-            position: absolute;
-            width: 20px;
-            height: 20px;
-            background-color: #3498db;
-            border-radius: 50%;
-        }
+          #vehicle {
+              position: absolute;
+              width: 5px;
+              height: 5px;
+              background-color: #3498db;
+              border-radius: 50%;
+              left: 50%;
+              top: 50%;
+              transform: translate(-50%, -50%);
+          }
 
-        .point {
-            position: absolute;
-            width: 5px;
-            height: 5px;
-            background-color: #e74c3c;
-            border-radius: 50%;
-        }     
+          .point {
+              position: absolute;
+              width: 5px;
+              height: 5px;
+              background-color: #e74c3c;
+              border-radius: 50%;
+          }
+
+          /* Ajoutez ces styles à votre fichier styles.css */
+
+          #arrow {
+              position: absolute;
+              width: 0;
+              height: 0;
+              border-left: 10px solid transparent;
+              border-right: 10px solid transparent;
+              border-bottom: 20px solid #e74c3c; /* Couleur de la flèche */
+              transform-origin: center top;
+          }
         </style>
     </head>
     <body>
-    <div id="map">
-        <div id="vehicle"></div>
-    </div>
+      <div id="map">
+          <div id="vehicle"></div>
+          <div id="arrow"></div> <!-- Ajoutez cette ligne pour la flèche -->
+          <div id="instructionDisplay">Déplacement en cours: </div>
+      </div>
     </body>
     <script>
-var vehicle = document.getElementById('vehicle');
-var map = document.getElementById('map');
 
-var currentPosition = { x: 0, y: 0 };
-var direction = 0; // 0: vers le haut, 1: vers la droite, 2: vers le bas, 3: vers la gauche
+      var vehicle = document.getElementById('vehicle');
+      var map = document.getElementById('map');
+      var instructionDisplay = document.getElementById('instructionDisplay');
+      var arrow = document.getElementById('arrow');
 
-var vehicleWidth = 20;
-var vehicleHeight = 20;
+      var currentPosition = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+      var direction = 0; // 0: vers le haut, 1: vers la droite, 2: vers le bas, 3: vers la gauche
 
-function moveVehicle(instruction) {
-    var step = 20;
+      var vehicleWidth = 5;
+      var vehicleHeight = 5;
 
-    function moveForward() {
-        switch (direction) {
-            case 0:
-                currentPosition.y = Math.max(currentPosition.y - step, 0);
-                break;
-            case 1:
-                currentPosition.x = currentPosition.x + step;
-                break;
-            case 2:
-                currentPosition.y = currentPosition.y + step;
-                break;
-            case 3:
-                currentPosition.x = Math.max(currentPosition.x - step, 0);
-                break;
-        }
-    }
+      function moveVehicle(instruction) {
+          var step = 5;
 
-    function moveBackward() {
-        switch (direction) {
-            case 0:
-                currentPosition.y = currentPosition.y + step;
-                break;
-            case 1:
-                currentPosition.x = Math.max(currentPosition.x - step, 0);
-                break;
-            case 2:
-                currentPosition.y = Math.max(currentPosition.y - step, 0);
-                break;
-            case 3:
-                currentPosition.x = currentPosition.x + step;
-                break;
-        }
-    }
+          function moveForward() {
+              switch (direction) {
+                  case 0:
+                      currentPosition.y -= step;
+                      break;
+                  case 1:
+                      currentPosition.x += step;
+                      break;
+                  case 2:
+                      currentPosition.y += step;
+                      break;
+                  case 3:
+                      currentPosition.x -= step;
+                      break;
+              }
+          }
 
-    function turnLeft() {
-        direction = (direction - 1 + 4) % 4; // Mettez à jour la direction vers la gauche
-    }
+          function moveBackward() {
+              switch (direction) {
+                  case 0:
+                      currentPosition.y += step;
+                      break;
+                  case 1:
+                      currentPosition.x -= step;
+                      break;
+                  case 2:
+                      currentPosition.y -= step;
+                      break;
+                  case 3:
+                      currentPosition.x += step;
+                      break;
+              }
+          }
 
-    function turnRight() {
-        direction = (direction + 1) % 4; // Mettez à jour la direction vers la droite
-    }
+          function turnLeft() {
+              direction = (direction - 1 + 4) % 4;
+          }
 
-    switch (instruction) {
-        case 'avancer':
-            moveForward();
-            break;
-        case 'reculer':
-            moveBackward();
-            break;
-        case 'gauche':
-            turnLeft();
-            break;
-        case 'droite':
-            turnRight();
-            break;
-        case 'arreter':
-            // Ne rien faire pour l'instruction 'arrêter'
-            break;
-        default:
-            console.error('Instruction non reconnue:', instruction);
-            return;
-    }
+          function turnRight() {
+              direction = (direction + 1) % 4;
+          }
 
-    createPoint(currentPosition.x, currentPosition.y);
-    adjustMapSize(currentPosition.x + vehicleWidth, currentPosition.y + vehicleHeight);
+          switch (instruction) {
+              case 'avancer':
+                  moveForward();
+                  break;
+              case 'reculer':
+                  moveBackward();
+                  break;
+              case 'gauche':
+                  turnLeft();
+                  break;
+              case 'droite':
+                  turnRight();
+                  break;
+              case 'arreter':
+                  break;
+              default:
+                  console.error('Instruction non reconnue:', instruction);
+                  return;
+          }
 
-    vehicle.style.left = currentPosition.x + 'px';
-    vehicle.style.top = currentPosition.y + 'px';
-}
+          createPoint(currentPosition.x, currentPosition.y);
+          adjustMapSize(Math.max(currentPosition.x + vehicleWidth, window.innerWidth), Math.max(currentPosition.y + vehicleHeight, window.innerHeight));
 
-function createPoint(x, y) {
-    var point = document.createElement('div');
-    point.className = 'point';
-    point.style.left = x + 'px';
-    point.style.top = y + 'px';
-    map.appendChild(point);
-}
+          // Met à jour la position et la rotation de la flèche
+          arrow.style.transform = 'rotate(' + (90 * direction) + 'deg)';
+          arrow.style.left = (currentPosition.x + vehicleWidth / 2 - 5) + 'px';
+          arrow.style.top = (currentPosition.y + vehicleHeight / 2 - 20) + 'px';
 
-function adjustMapSize(newWidth, newHeight) {
-    map.style.width = newWidth + 'px';
-    map.style.height = newHeight + 'px';
-}
+          // Met à jour la position du véhicule
+          vehicle.style.left = currentPosition.x + 'px';
+          vehicle.style.top = currentPosition.y + 'px';
+
+          instructionDisplay.textContent = 'Déplacement en cours: ' + instruction;
+      }
+
+      function createPoint(x, y) {
+          var point = document.createElement('div');
+          point.className = 'point';
+          point.style.left = x + 'px';
+          point.style.top = y + 'px';
+          map.appendChild(point);
+      }
+
+      function adjustMapSize(newWidth, newHeight) {
+          map.style.width = newWidth + 'px';
+          map.style.height = newHeight + 'px';
+      }
 
     setInterval(function(){ 
         fetch("/direction") 
