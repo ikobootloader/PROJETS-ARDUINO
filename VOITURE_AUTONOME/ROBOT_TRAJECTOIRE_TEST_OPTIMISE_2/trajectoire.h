96c2fca8599233747fd0959fb8ced3094049e64c
ikobootloader <63266419+ikobootloader@users.noreply.github.com>
2023-12-23 14:51:33 +0100

    MAJ TRAJECTOIRE
    Correction des bugs, ajustement des positions sur 360°
    Calcul trigo

--- VOITURE_AUTONOME/ROBOT_TRAJECTOIRE_TEST_OPTIMISE_2/trajectoire.h
+++ VOITURE_AUTONOME/ROBOT_TRAJECTOIRE_TEST_OPTIMISE_2/trajectoire.h
@@ -5,35 +5,35 @@
 //               POSITION DE L'AGENT              //
 ////////////////////////////////////////////////////
 struct Coordonnees {
-  float x;  // AVANCER ET RECULER
-  float y;  // GAUCHE ET DROITE
+  float x;  // 
+  float y;  // 
 };
-float axe = 0;           // AXE DE ROTATION de 0° à 360°
+float axe = 0; // AXE DE ROTATION 0° à 360°
 const float taillePas = 0.01;     // Taille du pas
 Coordonnees position = { 0, 0 };  // Initialiser les coordonnées à l'origine
 /***************************************************/
 
-void avancer(float degre) {
-  AVANCER;
+void avancer(float orientation) {
+  //AVANCER;
   i("avancer")  //COM WBSRVR
   // Conversion de degrés à radians
-  float angle_radian = PI * degre / 180.0;
+  float angle_radian = PI * orientation / 180.0;
   // Calcul des composantes x et y
-  float tailleX = taillePas * cos(angle_radian);
-  float tailleY = taillePas * sin(angle_radian);
+  float tailleY = taillePas * cos(angle_radian);
+  float tailleX = taillePas * sin(angle_radian);
   // Nouvelles positions
   position.x = position.x + tailleX;
   position.y = position.y + tailleY;
 }
 
-void reculer(float degre) {
-  RECULER;
+void reculer(float orientation) {
+  //RECULER;
   i("reculer")  //COM WBSRVR
   // Conversion de degrés à radians
-  float angle_radian = PI * degre / 180.0;
+  float angle_radian = PI * orientation / 180.0;
   // Calcul des composantes x et y
-  float tailleX = taillePas * cos(angle_radian);
-  float tailleY = taillePas * sin(angle_radian);
+  float tailleY = taillePas * cos(angle_radian);
+  float tailleX = taillePas * sin(angle_radian);
   // Nouvelles positions
   position.x = position.x - tailleX;
   position.y = position.y - tailleY;
@@ -47,9 +47,9 @@ float etalon = 0.288;
 
 void tournerDroite(float degre) {
   static int compteurD = 0;
-  degre = round(degre*etalon);//conversion
-  while(compteurD <= degre){
-    DROITE; 
+  float rotationParDegre = round(degre*etalon);//conversion
+  while(compteurD <= rotationParDegre){
+    //DROITE; 
     delay(30);
     ARRET;
     delay(100);
@@ -65,9 +65,9 @@ void tournerDroite(float degre) {
 
 void tournerGauche(float degre) {
   static int compteurG = 0;
-  degre = round(degre*etalon);//conversion
-  while(compteurG <= degre){
-    GAUCHE; 
+  float rotationParDegre = round(degre*etalon);//conversion
+  while(compteurG <= rotationParDegre){
+    //GAUCHE; 
     delay(30);
     ARRET;
     delay(100);
@@ -90,7 +90,7 @@ void arret() {
 ////////////////////////////////////////////////////
 //          SE DIRIGER VERS UNE POSITION          //
 ////////////////////////////////////////////////////
-bool rot = 0;
+
 // Fonction pour diriger l'agent vers des coordonnées spécifiées
 void allerA(float destinationX, float destinationY) {
   /***/
@@ -99,27 +99,74 @@ void allerA(float destinationX, float destinationY) {
   //hypoténuse √(→x² + →y²)
   /***/
   //Angle qui pointe vers la position de destination
-  float vecteurX = (position.x * -1) - (destinationX * -1);
-  float vecteurY = (position.y * -1) - (destinationY * -1);
-  unsigned short angleDestination = atan(vecteurX / vecteurY) * 180 / PI;
+  float vecteurX = abs((position.x * -1) - (destinationX * -1));
+  float vecteurY = abs((position.y * -1) - (destinationY * -1));
+
+  //Tangente
+  float angleDestination = atan2(vecteurX,vecteurY) * 180 / PI;
   //Trajectoire : hypoténuse
   float hypotenuse = sqrt(pow(vecteurX,2.0)+pow(vecteurY,2.0));
-  //Sur l'hypoténuse, perte de l'axe!
 
-  //TESTS
-  Serial.print("Vecteur y: ");i(vecteurY)
-  Serial.print("Vecteur x: ");i(vecteurX)
-  Serial.print("Angle destination: ");i(angleDestination)
-  Serial.print("Hypoténuse: ");i(hypotenuse)
- 
+  static bool rot = 0;
+
+  if(rot == 0){
+    //TESTS
+    Serial.print("Vecteur y: ");i(vecteurY)
+    Serial.print("Vecteur x: ");i(vecteurX)
+    Serial.print("Tangente en degré: ");i(angleDestination)
+    Serial.print("Hypoténuse: ");i(hypotenuse)
+    
+    //Triangulation
+    //(y,x)
+    if(destinationY > position.y && destinationX > position.x) angleDestination = angleDestination; 
+    //(-y,x)
+    if(destinationY < position.y && destinationX > position.x) angleDestination = 180 - angleDestination; 
+    //(-y,-x)
+    if(destinationY < position.y && destinationX < position.x) angleDestination = 180 + angleDestination; 
+    //(y,-x)
+    if(destinationY > position.y && destinationX < position.x) angleDestination = 360 - angleDestination; 
+
+    //pas de triangulation
+    //(y,x=x')
+    if(destinationY > position.y && destinationX == position.x) angleDestination = 0;
+    //(y=y',-x)
+    if(destinationY == position.y && destinationX < position.x) angleDestination = 270;
+    //(-y,x=x')
+    if(destinationY < position.y && destinationX == position.x) angleDestination = 180;
+    //(y=y',x)
+    if(destinationY == position.y && destinationX > position.x) angleDestination = 90;
+
+    Serial.print("Angle destination intégré : ");i(angleDestination)
+
+    //Quantité de degré vers l'angle de destination
+    float qteDegre = 0;
+    if(axe < angleDestination) qteDegre = angleDestination - axe;
+    if(axe > angleDestination) qteDegre = axe - angleDestination;
+
+    Serial.print("quantité de degré de rotation : ");i(qteDegre)
+
+    tournerDroite(qteDegre); //Tourner dans le sens horaire !
+
+/**
+    //Suivre hypoténuse
+    static long avancement = 0;
+    while(avancement <= hypotenuse){
+      avancer(axe);
+      avancement++;
+    }
+**/
+    ARRET;
+    rot = 1;
+    //avancement = 0;
+  }
+
 
 //Attention à retrouver en temps réel les positions x et y pour le webserveur !
-//WEBSERVER
-//Doit-on faire adapter la taille du pas : Taille du pas*√2 ????
-//Réelle distance : hypoténuse*(taille du pas *√2)/taille du pas ???? A tester !
+//=>Doit-on faire adapter la taille du pas : Taille du pas*√2 ????
+//=>Réelle distance : hypoténuse*(taille du pas *√2)/taille du pas ???? A tester !
 }
 
-//A adapter pour un déplacement sur l'hypoténuse
+//TODO
 //Pouvoir définir un territoire restreint en déplacement
 //garder en mémoire les obstacles ?
 //garder en mémoire les points de recharge ?
