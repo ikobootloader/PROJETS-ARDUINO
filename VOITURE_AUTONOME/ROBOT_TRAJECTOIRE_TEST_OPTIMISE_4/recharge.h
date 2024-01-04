d547a34787d09d998bc93b1a74d48bcbedd1a67e
Frédérick Murat <63266419+ikobootloader@users.noreply.github.com>
2024-01-04 18:39:58 +0100

    Add files via upload

--- VOITURE_AUTONOME/ROBOT_TRAJECTOIRE_TEST_OPTIMISE_4/recharge.h
+++ VOITURE_AUTONOME/ROBOT_TRAJECTOIRE_TEST_OPTIMISE_4/recharge.h
@@ -1,12 +1,14 @@
 #ifndef recharge_H
 #define recharge_H
 /***************************************************/
-bool pileOK = 0; //Interrupteur
+bool pileOK = 0; //Interrupteur d'activation du process de recharge
 float positionRecharge[3];//(x,y,volts)
+float tensionMinPPV = 4.5;
+float tensionMinBatterie = 7.5;
 
 void gestionPPV(){ //0
-  if(PPV.tensionAcomparer(4.5, A3, 0) == true && PPV.lecture_tension(A3) > positionRecharge[2]){
-    //enregistrer les positions de recharge + volts
+  if(PPV.tensionAcomparer(tensionMinPPV, A3, 0) == true && PPV.lecture_tension(A3) > positionRecharge[2]){
+    //enregistrer la meilleure position de recharge + volts
     positionRecharge[0] = position.x;
     positionRecharge[1] = position.y;
     positionRecharge[2] = PPV.lecture_tension(A3);
@@ -14,22 +16,20 @@ void gestionPPV(){ //0
 }
 
 void gestionBatterie(){ //1
-  if(batterie.tensionAcomparer(7.5, A6, 2) == true && pileOK == 0){
-    pileOK = 1;
-  }
+  //Si la batterie est trop faible, on active le process de recharge
+  if(batterie.tensionAcomparer(tensionMinBatterie, A6, 2) == true && pileOK == 0) pileOK = 1;
 }
 
 void pointRecharge(){ //2
   //Si état pileOK = 1 > GOTO recharge ou cherche recharge
-  if(positionRecharge[0] != position.x && positionRecharge[1] != position.y){
-    if(pileOK = 1 && positionRecharge[2] > 0){
-      allerA(positionRecharge[0],positionRecharge[1]);
-    }
-  }else if(batterie.lecture_tension(A6) >= 8.5){
-      pileOK = 0;
-  }else{
-
+  if(pileOK = 1 && positionRecharge[2] > 0){
+    if(positionRecharge[0] != position.x && positionRecharge[1] != position.y) allerA(positionRecharge[0],positionRecharge[1]);
+    if(positionRecharge[0] == position.x && positionRecharge[1] == position.y) ARRET;
   }
+  else{
+    avancer(axe); //Chercher un point de recharge
+  }
+  if(batterie.lecture_tension(A6) >= 8.5) pileOK = 0;
 }
 /***************************************************/
 #endif
